a111cd437ea03f1bfa5de0f4f0b446577ad1a96b
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-05-04 09:00:33 +0530

    Update rangechecker.c

--- rangechecker.c
+++ rangechecker.c
@@ -1,1 +1,20 @@
-agbveab
+#include <stdio.h>
+#include <cstdlib>
+#include "rangeChecker.h"
+
+int *sort(int *array, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
