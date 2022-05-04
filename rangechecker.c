164945c4b0aa7400a4542e07990f451c78055489
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-05-04 09:09:29 +0530

    Update rangechecker.c

--- rangechecker.c
+++ rangechecker.c
@@ -19,11 +19,32 @@ int *sort(int *array, int size)
     return array;
 }
 
-ReadingRangeList getReadingRangeList(int *ArrayofReadings, int size)
+ReadingsRangeList GetListofReadingsRange(int *ArrayofReadings, int size)
 {
     int *sortedArray = sort(ArrayofReadings, size);
-    ReadingRange *readingRanges = (ReadingRange *)malloc(size * sizeof(ReadingRange));
-    int start = sortedArray[0], end = sortedArray[0], count = 0, rangeIndex = 0;
+    ReadingRange *ReadingsRanges = (ReadingRange *)malloc(size * sizeof(ReadingRange));
+   int start = sortedArray[0], end = sortedArray[0], count = 0, rangeIndex = 0;
     for (int i = 0; i < size; i++)
     {
         if (sortedArray[i] == start || sortedArray[i] == end + 1)
+        {
+            count++;
+            end = sortedArray[i];
+        }
+        else
+        {
+            ReadingsRanges[rangeIndex] = {start, end, count};
+            rangeIndex++;
+            count = 1;
+            start = sortedArray[i];
+            end = start;
+        }
+    }
+    ReadingsRanges[rangeIndex] = {start, end, count};
+    ReadingsRangeList ListofReadingsRange = {ReadingsRanges, rangeIndex + 1};
+    for (int i = 0; i < ListofReadingsRange.totalReadingRanges; i++)
+    {
+        printf("%d-%d, %d\n", ReadingsRanges[i].rangeStart, ReadingsRanges[i].rangeEnd, ReadingsRanges[i].numberOfReadings);
+    }
+    return ListofReadingsRange;
+}
