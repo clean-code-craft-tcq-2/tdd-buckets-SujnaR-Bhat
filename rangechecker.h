499113601b7e95ed6be792c607fc7de6fdb97596
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-05-04 09:10:11 +0530

    Update rangechecker.h

--- rangechecker.h
+++ rangechecker.h
@@ -1,1 +1,13 @@
-bgdDg
+typedef struct ReadingRange {
+    int rangeStart;
+    int rangeEnd;
+    int numberOfReadings;
+} ReadingRange;
+
+typedef struct ReadingRangeList {
+    ReadingRange *readingRanges;
+    int totalReadingRanges;
+} ReadingRangeList;
+
+ReadingRangeList getReadingRangeList(int *readingsArray, int size);
+int *sort(int array, int size);
